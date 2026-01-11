a735429202369a8c46bfb77d1dd309f205855989
Shinichirou Yamazaki <shinichirou.yamazaki@gmail.com>
2026-01-11 14:41:10 +0900

    詳細画面の追加

---
+++ PhotosTest/Views/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  PhotosTest
+//
+//  Created by shinichirou on 2026/01/11.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
