57b16d8d12fcd84efef870add712b3ce70a5fd25
Shinichirou Yamazaki <shinichirou.yamazaki@gmail.com>
2026-01-31 14:54:57 +0900

    URL登録処理の追加

---
+++ PhotosTest/Views/EditView.swift
@@ -0,0 +1,18 @@
+//
+//  EditView.swift
+//  PhotosTest
+//
+//  Created by shinichirou on 2026/01/30.
+//
+
+import SwiftUI
+
+struct EditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditView()
+}
