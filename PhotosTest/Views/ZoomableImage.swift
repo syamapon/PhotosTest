a4694652f46518a523a11558c186e4573d979a94
Shinichirou Yamazaki <shinichirou.yamazaki@gmail.com>
2026-03-23 22:42:29 +0900

    拡大・縮小の実装

---
+++ PhotosTest/Views/ZoomableImage.swift
@@ -0,0 +1,18 @@
+//
+//  ZoomableImage.swift
+//  PhotosTest
+//
+//  Created by shinichirou on 2026/03/21.
+//
+
+import SwiftUI
+
+struct ZoomableImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ZoomableImage()
+}
