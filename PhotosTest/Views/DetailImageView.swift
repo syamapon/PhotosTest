a7325888b6bebe3e124d6340d7e4218de05cdae2
Shinichirou Yamazaki <shinichirou.yamazaki@gmail.com>
2026-05-24 16:55:53 +0900

    イメージ表示ビューを分割

---
+++ PhotosTest/Views/DetailImageView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailImageView.swift
+//  PhotosTest
+//
+//  Created by shinichirou on 2026/05/24.
+//
+
+import SwiftUI
+
+struct DetailImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailImageView()
+}
---
+++ PhotosTest/Views/DetailTextGridRow.swift
@@ -0,0 +1,18 @@
+//
+//  DetailTextGridRow.swift
+//  PhotosTest
+//
+//  Created by shinichirou on 2026/05/24.
+//
+
+import SwiftUI
+
+struct DetailTextGridRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailTextGridRow()
+}
