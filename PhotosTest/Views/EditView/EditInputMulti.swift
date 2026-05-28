547678cddba691fc911f495cce300d285e839b6e
Shinichirou Yamazaki <shinichirou.yamazaki@gmail.com>
2026-05-28 23:31:29 +0900

    Previewの追記

---
+++ PhotosTest/Views/EditView/EditInputMulti.swift
@@ -0,0 +1,18 @@
+//
+//  EditInputMulti.swift
+//  PhotosTest
+//
+//  Created by shinichirou on 2026/05/25.
+//
+
+import SwiftUI
+
+struct EditInputMulti: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditInputMulti()
+}
